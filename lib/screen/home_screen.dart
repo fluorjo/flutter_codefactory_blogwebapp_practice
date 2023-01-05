c34a35b514099b8fd16e8c1a901aca832a99b49b
fluorjo <fluorjo@naver.com>
2023-01-05 22:21:33 +0900

    앱바, 웹뷰

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Home Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text('Code Factory'),
+        centerTitle: true,
+      ),
+      body: const WebView(
+        initialUrl: 'https://blog.codefactory.ai',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
     );
   }
 }
